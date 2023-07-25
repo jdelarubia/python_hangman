1d298dbdacf5b55313239744c48eb29eb21f9648
jdelarubia <jdelarubia@gmail.com>
2023-07-25 21:41:15 +0200

    extract static method to gameprompts class

--- app/gameprompts.py
+++ app/gameprompts.py
@@ -33,3 +33,21 @@ class GamePrompts:
         name = input("What's your name? ")
         return name
 
+    @staticmethod
+    def choose_difficulty() -> str:
+        """Return the difficulty chosen by the user: easy, medium and hard word lists.
+
+        Returns:
+            str: choice
+        """
+
+        options = ["e", "m", "h"]
+        while True:
+            choice = input(
+                "Choose difficulty: e for easy. n for normal. h for hard. "
+            ).lower()[0]
+            if (not choice.isalpha()) or (choice not in options):
+                BColors.warning("That's not one of the options available. Try again.")
+                continue
+            break
+        return choice
